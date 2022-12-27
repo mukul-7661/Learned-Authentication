2e5ccbc4564aeeb955b7b854f5e480adaf9ce945
Mukul yadav <mukul7661@gmail.com>
2022-12-27 20:22:40 +0530

    Introduced Hashing to the authentication

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -26,7 +26,6 @@ async function main() {
 
     
 
-    userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
     const User = new mongoose.model("User", userSchema);
 
@@ -45,7 +44,7 @@ async function main() {
     app.post("/register", function(req, res){
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
 
         newUser.save(function(err){
@@ -60,7 +59,7 @@ async function main() {
 
     app.post("/login", function(req, res){
         const userName = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
 
         User.findOne({email: userName}, function(err, foundUser){
             if(err){
