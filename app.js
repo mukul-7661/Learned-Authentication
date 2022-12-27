1f67422f02da62ec722a9656a053cc5b7f4986ed
Mukul yadav <mukul7661@gmail.com>
2022-12-27 19:37:54 +0530

    Encryption using mongoose-encryption

---
+++ app.js
@@ -0,0 +1,82 @@
+
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+
+    const userSchema = new mongoose.Schema({
+        email: String,
+        password: String
+    });
+
+    const secret = "Thisisoursecret";
+
+    userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+    const User = new mongoose.model("User", userSchema);
+
+    app.get("/", function (req, res) {
+        res.render("home");
+    });
+
+    app.get("/login", function (req, res) {
+        res.render("login");
+    });
+
+    app.get("/register", function (req, res) {
+        res.render("register");
+    });
+
+    app.post("/register", function(req, res){
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+
+        newUser.save(function(err){
+            if(err) {
+                console.log(err);
+            }
+            else{
+                res.render("secrets");
+            }
+        });
+    });
+
+    app.post("/login", function(req, res){
+        const userName = req.body.username;
+        const password = req.body.password;
+
+        User.findOne({email: userName}, function(err, foundUser){
+            if(err){
+                console.log(err);
+            }
+            else{
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        })
+    });
+
+
+    app.listen(3000, function () {
+        console.log("Server is running");
+    });
+
+}
